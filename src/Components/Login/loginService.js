1443a0959370274f3f442a84f0203cf01a8ba9bd
Rafael Verissimo <123127434+Piucles@users.noreply.github.com>
2026-05-25 17:38:59 -0300

    Update loginService.js

--- src/Components/Login/loginService.js
+++ src/Components/Login/loginService.js
@@ -13,7 +13,15 @@ export const handleLogin = async (email, password) => {
     const data = await response.json();
 
     if (response.ok) {
+
+        // salva os tokens no localStorage
+        localStorage.setItem("access", data.access);
+        localStorage.setItem("refresh", data.refresh);
+
         alert("Login feito!");
+
+        console.log("TOKEN:", data.access);
+
     } else {
         alert(data.error);
     }
